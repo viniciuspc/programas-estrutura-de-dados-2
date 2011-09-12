f4bb1bedbdaa76ea067d1fe0f95350d86762a677
pasqualini9@gmail.com <pasqualini9@gmail.com@6eeb24be-5a52-d594-c498-2ae96b0477e5>
2011-09-12 10:59:25 +0000

    Matriz incidencia

---
+++ trunk/matrizincidecia.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+main()
+{
+        int m[5][5], a1,a2;
+        
+        printf("Digite o número do vertice");
+        return 0;  
+}
